4fef0f1b96caaa4a4ae0e52f73c5c271cff1bed1
Michael <micperdo@gmail.com>
2022-11-14 15:22:13 -0500

    created an array of sample data objects

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-alert("Hello World");
-
-let favFood = "arroz con pollo   ";
-document.write(favFood);
-favFood = "baked branzino over rice ";
-document.write(favFood);
+let pokemonList = [
+  {
+    name: "Noctowl",
+    height: 1.6,
+    attackType: ["Normal", "Flying"],
+    doubleDamage: ["Ice", "Electric", "Rock"],
+  },
+  {
+    name: "Typhlosion",
+    height: 1.7,
+    attackType: ["Fire"],
+    doubleDamage: ["Water", "Rock", "Ground"],
+  },
+  {
+    name: "Houndoom",
+    height: 1.4,
+    attackType: ["Dark", "Fire"],
+    doubleDamage: ["Bug", "Fighting", "Water", "Rock", "Ground"],
+  },
+  {
+    name: "Vileplume",
+    height: 1.2,
+    attackType: ["Grass", "Poison"],
+    doubleDamage: ["Ice", "Fire", "Flying", "Psychic", "Ground"],
+  },
+];
