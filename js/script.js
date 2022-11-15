e7eb2de18749996ff6509d97e2afc16f13e754c3
Michael <micperdo@gmail.com>
2022-11-15 11:18:20 -0500

    a for loop in script.js that loops over pokemon list and displays info to document

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+// sample data object.
 let pokemonList = [
   {
     name: "Noctowl",
-    height: 1.6,
+    height: 1.4,
     attackType: ["Normal", "Flying"],
     doubleDamage: ["Ice", "Electric", "Rock"],
   },
@@ -18,9 +19,38 @@ let pokemonList = [
     doubleDamage: ["Bug", "Fighting", "Water", "Rock", "Ground"],
   },
   {
+    name: "Meowth",
+    height: 0.4,
+    attackType: ["Normal"],
+    doubleDamage: ["Fighting"],
+  },
+  {
     name: "Vileplume",
     height: 1.2,
     attackType: ["Grass", "Poison"],
     doubleDamage: ["Ice", "Fire", "Flying", "Psychic", "Ground"],
   },
 ];
+/*
+  for loop that goes over every pokemon object in the pokemonList, checks it's height property against a 1.5 threshold using an if statement, and displays special info 
+  if the height threshold is met, or generic info if threshold is not met. It makes an h3
+  element of the poke name and p element of the height info. 
+*/
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1.5) {
+    document.write(`<hr>`);
+    document.write(
+      `<h3>${pokemonList[i].name}</h3> <p>(height: <span>${pokemonList[i].height}</span>) -- wow that's tall.</p>`
+    );
+  } else if (pokemonList[i].height < 0.5) {
+    document.write(`<hr>`);
+    document.write(
+      `<h3>${pokemonList[i].name}</h3> <p>(height: <span>${pokemonList[i].height}</span>) -- wow that's short.<p>`
+    );
+  } else {
+    document.write(`<hr>`);
+    document.write(
+      `<h3>${pokemonList[i].name}</h3> <p>(height:<span>${pokemonList[i].height}</span>)</p>`
+    );
+  }
+}
