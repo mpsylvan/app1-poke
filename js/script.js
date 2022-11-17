23c8d9b4c0b86a0c5a7f4ebfbbb84ce2a786c9bd
Michael <micperdo@gmail.com>
2022-11-17 16:41:21 -0500

    creates an addListItem() to display content to DOM and add event listeners

--- js/script.js
+++ js/script.js
@@ -57,6 +57,7 @@ let pokemonRepo = (function () {
       }
     }
   }
+
   //function to return the state of the list, if list is empty it console logs it's empty.
   function getAllPokemon() {
     if (pokemonList.length < 1) {
@@ -65,17 +66,46 @@ let pokemonRepo = (function () {
       return pokemonList;
     }
   }
+  // display pokemon object to the console.
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  // BONUS task,  create a function that handles event listener outside of addListItem()
+  function addClickEvent(button, pokemon) {
+    button.addEventListener("click", () => {
+      showDetails(pokemon);
+    });
+  }
+
+  // function to:
+  //    1) create a new li > button for the pokemon object passed in
+  //    2) attach the li > button to the ul list.
+  //    3) add a click event to button that displays pokemon to console using showDetails()
+  function addListItem(pokemon) {
+    let ul = document.querySelector(".poke-list");
+    let li_item = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("poke-btn");
+    li_item.appendChild(button);
+    ul.appendChild(li_item);
+    button.addEventListener("click", () => {
+      showDetails(pokemon);
+    });
+    addClickEvent(button, pokemon); // Bonus Task adds the event listener via separate function.
+  }
 
   return {
     add: add,
     getAllPokemon: getAllPokemon,
     // exposing the sample string just to get a feel for local vs global.
+    addListItem: addListItem,
     sampleString: sampleString,
   };
 })();
 
-console.log(pokemonRepo.sampleString);
-
+//testing the validation for specific object property format.
 pokemonRepo.add(1); // fails because it's number.
 pokemonRepo.add({ name: "charzard" }); //fails because it doesn't match the samplestring
 pokemonRepo.add("charzard"); // fails because it's string.
@@ -95,29 +125,30 @@ pokemonRepo.add({
   doubleDamage: ["Water", "Psychic"],
 });
 
-// pokemonRepo the pokemonList through getAllPokemon and then it calls forEach on the list.
-// In the call back function it writes information about each object to the document,
-// the information displayed is different depending on it's .height property.
-pokemonRepo.getAllPokemon().forEach((pokemon) => {
-  document.write(`<hr>`);
-  if (pokemon.height > 1.5) {
-    document.write(
-      `<h3>${pokemon.name}</h3> <p>(height: <span>${pokemon.height}</span>) -- wow that's tall.</p>`
-    );
-  } else if (pokemon.height < 0.5) {
-    document.write(
-      `<h3>${pokemon.name}</h3> <p>(height: <span>${pokemon.height}</span>) -- wow that's short.<p>`
-    );
-  } else {
-    document.write(
-      `<h3>${pokemon.name}</h3> <p>(height:<span>${pokemon.height}</span>)</p>`
-    );
-  }
+pokemonRepo.add({
+  name: "Pikachu",
+  height: 0.4,
+  attackType: ["Electric"],
+  doubleDamage: ["Water", "Psychic"],
 });
 
-// function that takes in a search name parameter, then it filters the pokemonlist checking if any pokemon
+pokemonRepo.add({
+  name: "Dog",
+  height: 1.0,
+  attackType: ["Normal"],
+  doubleDamage: ["Fire", "Psychic"],
+});
+
+// For each pokemon
+
+pokemonRepo.getAllPokemon().forEach((pokemon) => {
+  pokemonRepo.addListItem(pokemon);
+});
+
+// Bonus Task: function that takes in a search name parameter, then it filters the pokemonlist checking if any pokemon
 //have the name in lower case matching the searchName in lower case.
 //if search was successful it stores the search as result, if not it passes a string result saying no match.
+
 function getAPokemon(searchName) {
   let search = pokemonRepo
     .getAllPokemon()
@@ -131,25 +162,3 @@ function getAPokemon(searchName) {
 getAPokemon("noctowl"); // success
 getAPokemon("Michael"); // fail
 getAPokemon("mewtoo"); // success with 2 results.
-
-// Same result but by declaring displayPokemon externally and then
-// passing it as the parameter for pokemonRepo.forEach();
-
-// function displayPokemon(pokemon) {
-//   document.write(`<hr>`);
-//   if (pokemon.height > 1.5) {
-//     document.write(
-//       `<h3>${pokemon.name}</h3> <p>(height: <span>${pokemon.height}</span>) -- wow that's tall.</p>`
-//     );
-//   } else if (pokemon.height < 0.5) {
-//     document.write(
-//       `<h3>${pokemon.name}</h3> <p>(height: <span>${pokemon.height}</span>) -- wow that's short.<p>`
-//     );
-//   } else {
-//     document.write(
-//       `<h3>${pokemon.name}</h3> <p>(height:<span>${pokemon.height}</span>)</p>`
-//     );
-//   }
-// }
-
-// pokemonRepo.getAllPokemon().forEach(displayPokemon);
